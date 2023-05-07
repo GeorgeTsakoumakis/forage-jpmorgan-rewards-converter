420cd1ae0e8f35bdd50db944d264d76120e36091
George Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2023-05-07 21:08:40 +0100

    Add RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,28 @@
+public class RewardValue {
+    private double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getMilesValue() {
+        return ConvertCashToMiles(this.cashValue);
+    }
+
+    private static double ConvertMilesToCash(int milesValue) {
+        return (milesValue * MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static int ConvertCashToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+}
