95b443a9b2cc9b287cd76f1e497b4a1b97f27c20
George Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2023-05-08 23:12:57 +0100

    fix merge

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,21 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        // Test 1: $100 = 28571 miles
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int miles = rewardValue.getMilesValue();
+        assertEquals(28571, miles);
+        assertEquals(0.0035, RewardValue.MILES_TO_CASH_CONVERSION_RATE);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        // Test 2: 10000 miles = $35
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double cash = rewardValue.getCashValue();
+        assertEquals(35, cash);
+        assertEquals(0.0035, RewardValue.MILES_TO_CASH_CONVERSION_RATE);
     }
 }
